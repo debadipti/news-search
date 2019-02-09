daa105440e16312cfcd07f63265d93277ecf3722
Debadipti Patra <pdebadipti95@gmail.com>
2019-02-09 20:52:52 +0530

    auto-refresh working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,41 @@
 // dom references
-const form = document.querySelector("#form");
 const input = document.querySelector("#input_field");
+const submitBtn = document.querySelector("#submit");
 const newsFeed = document.querySelector(".news-feed");
 
 // get the input field in focuswhen the page loads
 input.focus();
 
 // event listener for the form
-form.addEventListener("submit", getNews);
+submitBtn.addEventListener("click", getNews);
 
 // api and url information
 const apiKey = "363d26dd3d664d199ca63adc371e22aa";
 const url = "https://newsapi.org/v2/everything";
 
 // getNews()
-function getNews(e) {
-  // prevent submitting to the form
-  e.preventDefault();
-
+function getNews() {
+  // fetch params
   const query = `?q=${input.value}`;
   const pageSize = 10;
   const page = 1;
+
+  // input value
+  let inputValue = input.value;
 
   // request endpoint
   const endpoint = `${url}${query}&apiKey=${apiKey}&pageSize=${pageSize}&page=${page}`;
 
   // checking the input field in not blank
-  if (input.value !== "") {
+  if (inputValue) {
     // fetch api
     fetch(endpoint)
       .then(
         response => {
           // check if response returned with ok status
           if (response.ok) {
-            // clear the input field
-            input.value = "";
-            input.focud = false;
+            // focus the field
+            input.focus = false;
             // convert to json
             return response.json();
           }
@@ -47,12 +47,22 @@ function getNews(e) {
       )
       .then(jsonResponse => {
         const newsArray = jsonResponse.articles;
+
         // rendernews function to render the news in the DOM
         renderNews(newsArray);
+
+        refresh();
       });
   } else {
     newsFeed.innerHTML = `<h2>Please type something</h2>`;
   }
+}
+
+function refresh() {
+  setTimeout(() => {
+    getNews();
+    console.log("loaded");
+  }, 5000);
 }
 
 // renderNews()
